2739ff911555e0032029e56fdf3d2b546cef063f
Abhilash Bhosle <abhilashbhosle10@gmail.com>
2024-11-07 15:55:34 +0530

    added multiple files and tested with meshlabs outputs

--- Stl.js
+++ Stl.js
@@ -9,9 +9,10 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 10;
+camera.position.z = 20;
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias:true}); // antialias is used to render smooth edges..
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -25,6 +26,10 @@ scene.add(directionalLight);
 
 // Create orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // Enable smooth damping (inertia)
+controls.dampingFactor = 0.05; // The amount of damping
+controls.screenSpacePanning = false; // Disable panning with right-click
+// controls.maxPolarAngle = Math.PI / 2; // Limit vertical rotation
 
 // Array of STL file paths
 const stlFiles = [
@@ -51,6 +56,24 @@ const stlFiles = [
   "/models/Nuclei00021.stl",
   "/models/Nuclei00022.stl",
   "/models/Nuclei00024.stl",
+  "/models/Brain_stem00001.stl",
+  "/models/Brain_stem00002.stl",
+  "/models/Brain_stem00003.stl",
+  "/models/Brain_stem00004.stl",
+  "/models/Brain_stem00005.stl",
+  "/models/Brain_stem00006.stl",
+  "/models/Brain_stem00007.stl",
+  "/models/Brain_stem00008.stl",
+  "/models/Brain_stem00009.stl",
+  "/models/Brain_stem00010.stl",
+  "/models/Brain_stem00011.stl",
+  "/models/Brain_stem00012.stl",
+  "/models/Brain_stem00013.stl",
+  "/models/Brain_stem00014.stl",
+  "/models/Brain_stem00015.stl",
+  "/models/Brain_stem00016.stl",
+  "/models/Brain_stem00017.stl",
+  "/models/Brain_stem00018.stl",
 ];
 
 // Load each STL file
@@ -58,13 +81,15 @@ const loader = new STLLoader();
 stlFiles.forEach((filePath) => {
   loader.load(filePath, function (geometry) {
     const material = new THREE.MeshStandardMaterial({
-      color: Math.random() * 0xffffff,
+      // color: Math.random() * 0xffffff,
+      color: 0xffffff,
     }); // Random color for each model
     const mesh = new THREE.Mesh(geometry, material);
 
     // Center the geometry
     geometry.center();
-    mesh.scale.set(0.2, 0.2, 0.2); // Adjust the scale as necessary
+    mesh.scale.set(0.15, 0.15, 0.15); // Adjust the scale as necessary
+    // mesh.scale.set(1, 1, 1); // Adjust the scale as necessary
 
     scene.add(mesh);
 
